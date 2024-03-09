9a3e505aae71e215e78830b0ada034b5810b42d0
satyam70288 <satyamb971@gmail.com>
2024-03-09 16:15:28 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./pages/home"
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Home/>
+    </h1>
+  )
+}
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  Portfolio from "../components/portfolio"
+
+const home = () => {
+  return (
+    <div>home
+        <Portfolio/>
+    </div>
+  )
+}
+
+export default home
