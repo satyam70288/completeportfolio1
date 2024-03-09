104e558dd59eb3d993627c3e2cbdd80e41693a19
satyam70288 <satyamb971@gmail.com>
2024-03-09 16:57:01 +0530

    routes setup

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 const cors = require("cors");
 const { connectDb } = require("./connection");
+const routes = require("./routes/routes");
 
 connectDb();
 // middelware
@@ -11,6 +12,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
+
+app.use("/api", routes);
 
 app.listen(port, (req, res) => {
   console.log(`server listening on port ${port}`);
